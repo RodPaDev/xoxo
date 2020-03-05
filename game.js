9f6fe9dc660544d194c52a586268adb4cfdf0d1e
Patrick Rodrigues <rodpadev@rodriguespatrick.com>
2020-03-05 15:18:25 +0100

    add game class & functioality

--- game.js
+++ game.js
@@ -1,19 +1,73 @@
-class GameCanvas extends Game {
+"use strict";
+
+class Game {
+  constructor(P1, P2, regions, msgElm) {
+    this.P1 = P1;
+    this.P2 = P2;
+    this.regions = regions;
+    this.msgElm = msgElm;
+    this.rounds = 0;
+    this.currentPlayer = this.P1; //this will be set to either P1 or P2
+    this.playedRegions = []
+  }
+
+  msg() {
+    if(this.rounds >= 9){
+      this.msgElm.textContent = `XOXO it's a bloody draw`
+      return;
+    }
+    this.msgElm.textContent = `${this.currentPlayer.name}'s turn`
+  
+  }
+
+  checkIfAlreadyPlayed(region){
+    if(this.playedRegions.includes(region)){
+      return
+    }
+  }
+
+  play(event) {
+    let x = event.layerX;
+    let y = event.layerY;
+    console.log("x:", x, "\ny:", y);
+    
+    for(let region of this.regions){
+      if( x <= region[0] && y <= region[1]) {
+        if(this.playedRegions.includes(region)){
+          return
+        }else{
+          this.playedRegions.push(region)
+        }
+
+        this.rounds++
+        // console.log(`x: ${x}, y: ${y}\nIs in x: ${region[0]}, y: ${region[1]}`);
+        this.currentPlayer.drawSymbol(region)
+        this.currentPlayer = this.currentPlayer === this.P1 ? this.P2 : this.P1
+        this.msg()
+        return;
+      }
+    }
+  }
+
+}
+
+class GameCanvas {
   constructor(width, height, canvas) {
     this.width = width;
     this.height = height;
     this.canvas = canvas;
 
-    canvas.width = this.width;
-    canvas.height = this.height;
+    canvas.width = width;
+    canvas.height = height;
 
     this.context = this.canvas.getContext("2d");
   }
 }
 
-class Grid extends GameCanvas {
-  constructor(columns, width, context) {
-    super(width, context);
+class Grid {
+  constructor( width, context, columns) {
+    this.width = width;
+    this.context = context;
     this.columns = columns;
     this.columnSize = Math.floor(width / columns);
     this.regions = [];
@@ -34,7 +88,7 @@ class Grid extends GameCanvas {
     for (let j = this.columnSize; j < size; j += this.columnSize) {
       this.context.beginPath();
       this.context.moveTo(lineOffest, j);
-      this.context.lineTo(this.height - lineOffest, j);
+      this.context.lineTo(this.width - lineOffest, j);
       this.context.stroke();
     }
     this.context.save();
@@ -56,9 +110,11 @@ class Grid extends GameCanvas {
     return;
   }
 }
-class Player extends GameCanvas {
+class Player {
   constructor(name, symbol, isPlayerCPU = false, context, columnSize, regions) {
-    super(context, columnSize, regions);
+    this.context = context;
+    this.columnSize = columnSize;
+    this.regions = regions;
     this.name = name;
     this.symbol = symbol;
     this.isPlayerCPU = isPlayerCPU;
@@ -66,14 +122,15 @@ class Player extends GameCanvas {
     this.currentRegion = [];
   }
 
-  drawSymbol() {
+  drawSymbol(region) {
+    this.currentRegion = region;
     const halfColumn = this.columnSize / 2;
     const x = this.currentRegion[0] - halfColumn;
-    const y = this.currentRegion[0] - halfColumn;
+    const y = this.currentRegion[1] - halfColumn;
 
     if (this.symbol.toLowerCase() === "o") {
       this.context.beginPath();
-      this.context.arc(x, z, 20, 0, 2 * Math.PI);
+      this.context.arc(x, y, 20, 0, 2 * Math.PI);
       this.context.stroke();
       return;
     }
@@ -104,11 +161,17 @@ class Plays {
     return;
   }
 }
+const canvas = document.getElementById("xoxo-canvas");
+const canvasInstance = new GameCanvas(400,400, canvas);
+const gridInstance = new Grid(canvasInstance.width, canvasInstance.context, 3)
+gridInstance.drawGrid(25)
+gridInstance.regionMappers()
+const player_1 = new Player("P1", "X", false, canvasInstance.context, gridInstance.columnSize, gridInstance.regions)
+const player_2 = new Player("P2", "O", false, canvasInstance.context, gridInstance.columnSize, gridInstance.regions)
 
-class Game {
-  constructor(P1, P2) {
-    this.P1 = P1;
-    this.P2 = P2;
-    this.currentPlayer = null; //this will be set to either P1 or P2
-  }
-}
+const msgElement = document.getElementById("msg")
+const game = new Game(player_1, player_2, gridInstance.regions, msgElement)
+
+canvas.addEventListener('click', event =>{
+  game.play(event)
+}, false)
